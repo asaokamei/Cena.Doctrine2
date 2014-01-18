2dd3adff4ec7ae0f18d70863dcf4ec68584e623e
Asao <kamei@workspot.jp>
2014-01-18 16:58:08 +0900

    create CenaManager in the test.

--- tests/Tests/Cm_BasicTest.php
+++ tests/Tests/Cm_BasicTest.php
@@ -1,2 +1,40 @@
 <?php
-namespace Tests;
+namespace Tests\Tests;
+
+use Cena\Cena\CenaManager;
+use Cena\Cena\Collection;
+use Cena\Cena\Composition;
+use Cena\Cena\EmAdapterInterface;
+use Cena\Doctrine2\EmaDoctrine2;
+
+class Cm_BasicTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var EmAdapterInterface
+     */
+    public $ema;
+
+    /**
+     * @var CenaManager
+     */
+    public $cm;
+
+    function setUp()
+    {
+        $em = include( __DIR__ . '/../../bootstrap.php' );
+
+        $this->ema = new EmaDoctrine2();
+        $this->ema->setEntityManager( $em );
+
+        $this->cm = new CenaManager(
+            new Composition(),
+            new Collection()
+        );
+        $this->cm->setEntityManager( $this->ema );
+    }
+    
+    function test0()
+    {
+        $this->assertEquals( 'Cena\Cena\CenaManager', get_class( $this->cm ) );
+    }
+}
