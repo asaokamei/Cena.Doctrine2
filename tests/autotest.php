f8a2d05f9281f3d0486c4a87291d4bbbbdb2022a
Asao <kamei@workspot.jp>
2014-01-18 17:12:05 +0900

    addPsr4 for Tests classes.

--- tests/autotest.php
+++ tests/autotest.php
@@ -9,6 +9,7 @@ require_once( dirname( __DIR__ ) . '/vendor/autoload.php' );
 $loader = include( dirname( __DIR__ ) . '/vendor/autoload.php' );
 
 $loader->addPsr4( 'Cena\\Doctrine2\\', dirname( __DIR__ ) .'/src' );
+$loader->addPsr4( 'Tests\\', __DIR__ );
 $loader->register();
 
 // Create a simple "default" Doctrine ORM configuration for Annotations
